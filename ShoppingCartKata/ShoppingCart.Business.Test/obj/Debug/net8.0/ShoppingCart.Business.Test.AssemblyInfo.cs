f94b8f3d6e77393e4590d8afa46c492ccd01df95
Daniel Diaz <dadiaz@aidacanarias.com>
2024-10-30 09:52:39 +0000

    nunit and fluentassertions packages added to business.test project dependencies

--- ShoppingCartKata/ShoppingCart.Business.Test/obj/Debug/net8.0/ShoppingCart.Business.Test.AssemblyInfo.cs
+++ ShoppingCartKata/ShoppingCart.Business.Test/obj/Debug/net8.0/ShoppingCart.Business.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart.Business.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafbe2f47ac8316ffeb760d9af70c4fffe3f63e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e575ddddf4cf78836fb7ffd8b017390d7cb044")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart.Business.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart.Business.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
